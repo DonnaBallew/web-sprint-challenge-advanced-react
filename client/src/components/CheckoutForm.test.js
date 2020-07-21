5e01c98a216f917919dba7531bd832afe1f8c00e
Donna Ballew <ballewdd@gmail.com>
2020-07-20 21:32:38 -0700

    working on test

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  render,
-  fireEvent,
-  screen,
-  getByLabelText,
-} from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -18,29 +13,42 @@ test("form header renders", () => {
 });
 
 test("form shows success message on submit with form details", () => {
-  render(<CheckoutForm />);
+  const { getByLabelText, getByText, getByTestId, getByDisplayValue } = render(
+    <CheckoutForm />
+  );
 
-  const firsName = screen.getByLabelText(/first name/i);
-  const lastName = screen.getByLabelText(/last name/i);
-  const address = screen.getByLabelText(/address/i);
-  const city = screen.getByLabelText(/city/i);
-  const state = screen.getByLabelText(/state/i);
-  const zip = screen.getByLabelText(/zip/i);
-  const button = screen.getByLabelText(/Checkout/i);
+  const firstName = getByLabelText(/first name/i);
+  const lastName = getByLabelText(/last name/i);
+  const address = getByLabelText(/address/i);
+  const city = getByLabelText(/city/i);
+  const state = getByLabelText(/state/i);
+  const zip = getByLabelText(/zip/i);
 
-  fireEvent.change(firsName, { target: { value: "Donna" } });
+  expect(firstName).toBeInTheDocument();
+  expect(lastName).toBeInTheDocument();
+  expect(address).toBeInTheDocument();
+  expect(city).toBeInTheDocument();
+  expect(state).toBeInTheDocument();
+  expect(zip).toBeInTheDocument();
+
+  fireEvent.change(firstName, { target: { value: "Donna" } });
   fireEvent.change(lastName, { target: { value: "Ballew" } });
   fireEvent.change(address, { target: { value: "6547 N Academy Blvd, 1091" } });
   fireEvent.change(city, { target: { value: "Colorado Springs" } });
-  fireEvent.change(state, { target: { value: "Colorado" } });
+  fireEvent.change(state, { target: { value: "CO" } });
   fireEvent.change(zip, { target: { value: "80918" } });
-  fireEvent.click(button);
 
-  const grabName = screen.getByText(/Donna/i);
-  expect(grabName).toBeInTheDocument();
+  expect(getByDisplayValue(/Donna/i)).toBeInTheDocument();
+  expect(getByDisplayValue(/Ballew/i)).toBeInTheDocument();
+  expect(getByDisplayValue(/6547 N Academy Blvd, 1091/i)).toBeInTheDocument();
+  expect(getByDisplayValue(/Colorado Springs/i)).toBeInTheDocument();
+  expect(getByDisplayValue(/CO/i)).toBeInTheDocument();
+  expect(getByDisplayValue(/80918/i)).toBeInTheDocument();
 
-  const success = screen.getByLabelId(/successMessage/i);
+  const checkoutSubmit = getByTestId(/submitCheckout/i);
+  expect(checkoutSubmit).toBeInTheDocument();
+  fireEvent.click(checkoutSubmit);
+
+  const success = getByTestId(/successMessage/i);
   expect(success).toBeInTheDocument();
 });
-
-// UNABLE TO RUN TEST!!!
